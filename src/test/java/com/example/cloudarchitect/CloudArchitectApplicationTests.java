de9c7316bbec38daa3fb7130cedb1602da631092
perfect-Bee <joonslee3@gmail.com>
2026-02-03 04:13:57 +0900

    init

---
+++ src/test/java/com/example/cloudarchitect/CloudArchitectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudarchitect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudArchitectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
